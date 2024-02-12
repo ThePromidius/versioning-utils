8d90da2ef6492539858fa031b2a5dcb5aa7de1d2
ThePromidius <thepromidiusyt@gmail.com>
2024-02-12 13:41:57 +0100

    bump

--- src/versioning_utils/utils/git.py
+++ src/versioning_utils/utils/git.py
@@ -20,7 +20,7 @@ def _get_latest_commit_hash(src_path, package_name):
     try:
         # get latest commit where the module was changed excluding the __version__.py file
         git_command = f'git log -1 --pretty=format:%H -- {src_path}/{package_name} ":^{src_path}/{package_name}/__version__.py"'
-        result = subprocess.run(git_command, shell=True, capture_output=True, text=True, check=True)
+        result = subprocess.run(git_command, shell=True, capture_output=True, text=True, check=True,cwd=".")
         result_ = result.stdout.split("\n")[0].strip()
         return result_
     except subprocess.CalledProcessError as e:
@@ -31,7 +31,7 @@ def _get_latest_commit_hash(src_path, package_name):
         return None
 
 
-def has_code_changed(source_folder: str, package_name: str, latest_hash: str) -> bool | str:
+def has_code_changed(source_folder: str, package_name: str, version_metadata: dict) -> bool | str:
     """
     Check if the code of the package has been modified since last saved commit.
     Compares the latest git commit hash with the saved commit hash.
@@ -49,9 +49,10 @@ def has_code_changed(source_folder: str, package_name: str, latest_hash: str) ->
         False: bool - If the code has not changed
 
     """
-
+    saved_commit_hash = version_metadata.get(package_name,{}).get("commit_hash",None)
     latest_commit_hash = _get_latest_commit_hash(source_folder, package_name)
-    if latest_hash and latest_hash == latest_commit_hash:
+    print(f"Commit: {latest_commit_hash} vs Saved:{saved_commit_hash}")
+    if latest_commit_hash and saved_commit_hash == latest_commit_hash:
         return False
     else:
         return latest_commit_hash
